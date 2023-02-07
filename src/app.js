20e43534f0b4b2809d08069e6bbb91fb8abfe03b
Andy Li <andy@Andys-Air.hitronhub.home>
2023-02-07 17:17:22 -0500

    Initial files to start server service

---
+++ src/app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
+// const passport = require('passport');
+// const authenticate = require('./authorization/index');
+const { createErrorResponse } = require('./response');
+
+// const { version, author } = require('../package.json');
+
+// const logger = require('./logger');
+// const pino = require('pino-http')({
+//   // Use our default logger instance, which is already configured
+//   logger,
+// });
+
+const app = express(); //routes
+// app.use(pino); //logging
+app.use(helmet()); //security
+app.use(cors()); //x-origin
+app.use(compression()); //compression for middlware
+// passport.use(authenticate.strategy());
+// app.use(passport.initialize());
+
+app.use('/', require('./routes'));
+
+// app.get('/', (req, res) => {
+//   // Clients shouldn't cache this response (always request it fresh)
+//   res.setHeader('Cache-Control', 'no-cache');
+//   // Send a 200 'OK' response with info about our repo
+//   res.status(200).json({
+//     status: 'ok',
+//     author,
+//     githubUrl: 'https://github.com/Xydan/fragments',
+//     version,
+//   });
+// });
+
+app.use((req, res) => {
+  res.status(404).json(createErrorResponse(
+     404, 'not found'
+  ));
+});
+
+// app.use((err, req, res) => {
+//   const status = err.status || 500;
+//   const message = err.message || 'unable to process request';
+
+//   if (status > 499) {
+//     logger.error({ err }, `Error processing request`);
+//   }
+
+//   res.status(status).json({
+//     status: 'error',
+//     error: {
+//       message,
+//       code: status,
+//     },
+//   });
+// });
+
+module.exports = app;
---
+++ src/routes/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const { version, author } = require('../../package.json');
+const {Joseph} = require('../sample/profile-person.json');
+const {Maggie} = require('../sample/profile-pet.json');
+// const { authenticate } = require('../authorization/index');
+const {createSuccessResponse  } = require('../response');
+
+// Create a router that we can use to mount our API
+const router = express.Router();
+
+/**
+ * Expose all of our API routes on /v1/* to include an API version.
+ */
+router.use(`/v1`, require('./api'));
+
+/**
+ * Define a simple health check route. If the server is running
+ * we'll respond with a 200 OK.  If not, the server isn't healthy.
+ */
+router.get('/', (req, res) => {
+  // Client's shouldn't cache this response (always request it fresh)
+  res.setHeader('Cache-Control', 'no-cache');
+  // Send a 200 'OK' response
+  res.status(200).json(createSuccessResponse({author,githubUrl: 'https://github.com/MDXCODE/animal-aid-back', version}));
+});
+
+router.get('/profile/person/joseph', (req, res)=>{
+  res.status(200).json(createSuccessResponse(Joseph));
+});
+
+router.get('/profile/pet/maggie', (req, res)=>{
+  res.status(200).json(createSuccessResponse(Maggie));
+});
+
+module.exports = router;
