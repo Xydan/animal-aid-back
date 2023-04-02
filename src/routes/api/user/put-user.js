d0cbcda8366989fa834315b85f8ac4c93af2a756
Andy M Li <andym.li1995@gmail.com>
2023-04-02 17:49:53 -0400

    refactor put user route

--- src/routes/api/user/put-user.js
+++ src/routes/api/user/put-user.js
@@ -1,23 +1,28 @@
 const {createSuccessResponse} = require('../../../response');
 const {createErrorResponse} = require('../../../response');
 const con = require('../mysql');
+const bcrypt = require("bcrypt");
 
 module.exports = (req, res)=>{
     var fName= req.body.first_name;
     var lName = req.body.last_name;
     var desc = req.body.description;
     var image = req.body.profile_image;
-    var email = req.body.email; //pk
+    var email = req.body.email; 
+    var password = req.body.password;
+
 
     if(!email){
         res.status(404).json(createErrorResponse(404, "Email cannot be empty"));
     }else{
-        con.query(`UPDATE user SET first_name = '${fName}', last_name = '${lName}', description = '${desc}', profile_image = '${image}' WHERE email = '${email}'`, (err, result)=>{
-            if(err){
-                res.status(404).json(createErrorResponse(404, err));
-            }else{
-                res.status(200).json(createSuccessResponse(result));
-            }
+        bcrypt.hash(password, 10, function(err, hash) {
+            con.query(`UPDATE user SET first_name = '${fName}', last_name = '${lName}', description = '${desc}', profile_image = '${image}', password = '${hash}' WHERE email = '${email}'`, (err, result)=>{
+                if(err){
+                    res.status(404).json(createErrorResponse(404, err));
+                }else{
+                    res.status(200).json(createSuccessResponse(result));
+                }
+            });
         });
     }
 }
